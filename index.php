aae7d5fdb670dba38ea969fb52853f709ad1367f
Tom Creagh <tomcreagh@Toms-iMac.local>
2018-06-03 17:00:15 +1200

    bolstering

--- index.php
+++ index.php
@@ -1,103 +1,91 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>SmartHR - Human Resources Consulting</title>
-	<meta name="description=" content="">
-	<link rel="stylesheet" type="text/css" href="SmartHR.css">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="DIYCSS.css">
 </head>
+
 <body>
-<!-- Navigation -->
 
-	<div class="sandwichIcon">
-		<div class="barOne"></div>
-		<div class="barTwo"></div>
-		<div class="barThree"></div>
+	<div class="SplashContainer">
+
+		<!-- menu navigation and navigation list screen -->
+
+		<div class="navigationContainer">
+			<div class="navigationClose">
+				<div class="lineL"></div>
+				<div class="lineR"></div>
+			</div>
+			<p class="savedSelections navLinks"><b>Saved<br>Selections</b></p>
+			<p class="profile navLinks"><b>Profile</b></p>
+			<p class="settings navLinks"><b>Settings</b></p>
+		</div>
+		<div class="navigationHeader">
+			<h2>Menu</h2>
+		</div>
+
+		<div class="settingsContainer">
+			<label> Font size:</label>
+			<input class="textSize" type="range" name="textSize" min="12" max="20" onchange="textSize(this.value)"></input>
+		</div>
+
+		<!-- Log in page section -->
+
+		<div id="LogInContainer">
+			<h1>Name!</h1>
+			<p>Find and share assistive creations.</p>
+			<h2>Log In</h2>
+
+			<form class='LogIn' action='' method='post'>
+				<input name="username" type="text">
+				<label for="username">Username:</label>
+				
+				<input name="password" type="password">
+				<label for="password">Password:</label>
+
+				<input type="submit" value="Done" name="LogIn">
+
+			</form>
+
+			<p>Don't have an account?   <a id="RegisterPageLink" href="javascript:;">Make one.</a></p>
+
+		</div>
+
+		<!-- register page section -->
+
+		<div id="RegisterContainer">
+			<h2>Register</h2>
+
+			<form enctype="multipart/form-data" class='Register' action='' method='post'>
+
+				<input class="fName" name="fName" type="text">
+				<label>What's your name, friend?</label>
+
+				<input class="usernameR" name="usernameR" type="text">
+				<label>What'll your username be?</label>
+				
+				<input class="passwordR" name="passwordR" type="password">
+				<label>What'll your password be?</label>
+				
+				<input name="password1R" type="password">
+				<label>Type your password once more.</label>
+				
+				<input type="submit" value="Make me an account!" name="register">
+
+			</form>
+
+			<p id="LogInReturnLink">Changed your mind?   <a href="javascript:;">Take me back.</a></p>
+		</div>
+
+		<!-- menu screen and content screen switch -->
+
+		<div class="browseFooter">
+			<h2>Browse</h2>
+		</div>
+
 	</div>
 
-	<div class="menuBar"></div>
-
-	<div class="links">
-		<ul class="links">
-			<li class="linkOne"></li>
-			<li class="linkTwo"></li>
-			<li class="linkThree"></li>
-		</ul>
-	</div>
-
-	<div class="navigationClose">
-		<div class="lineL"></div>
-		<div class="lineR"></div>
-	</div>
-
-	<div class="returnToTop">
-		<img src="SmartHR_ReturnToTopButton.svg" alt="An arrow pointing upwards inside a circle.">
-	</div>
-
-<!-- Introductory Section -->
-
-	<h1>SmartHR</h1>
-	<h2>Raylee Marfell</h2>
-
-	<div class="SVGImage">
-		<?xml version="1.0" encoding="utf-8"?>
-		<!-- Generator: Adobe Illustrator 22.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-		viewBox="0 0 750 222.3" style="enable-background:new 0 0 750 222.3;" xml:space="preserve">
-		<style type="text/css">
-		.st0{fill:#1B1464;}
-		.st1{fill:url(#SVGID_1_);}
-		.st2{fill:url(#SVGID_2_);}
-		.st3{fill:url(#SVGID_3_);}
-	</style>
-	<path class="st0" d="M0,103.4c0,0,174.5,34.2,319,3.8c200-42,431-2,431-2v117H0V103.4z"/>
-	<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="748" y1="204.9224" x2="1067" y2="204.9224" gradientTransform="matrix(-1 0 0 -1 1067 284.8208)">
-		<stop  offset="0" style="stop-color:#0760C5"/>
-		<stop  offset="0.2999" style="stop-color:#085EC2"/>
-		<stop  offset="0.4902" style="stop-color:#0A58BA"/>
-		<stop  offset="0.6499" style="stop-color:#0F4EAB"/>
-		<stop  offset="0.7927" style="stop-color:#154096"/>
-		<stop  offset="0.9231" style="stop-color:#1C2E7C"/>
-		<stop  offset="1" style="stop-color:#222168"/>
-	</linearGradient>
-	<path class="st1" d="M319,107.3c0,0-214,11-319-102.5c0,94.5,0,112.7,0,112.7S129,208.3,319,107.3z"/>
-	<linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="319" y1="140.5851" x2="750.4445" y2="140.5851">
-		<stop  offset="0" style="stop-color:#0760C5"/>
-		<stop  offset="0.109" style="stop-color:#0C54B4"/>
-		<stop  offset="0.3565" style="stop-color:#153E93"/>
-		<stop  offset="0.5936" style="stop-color:#1C2E7B"/>
-		<stop  offset="0.8137" style="stop-color:#21246D"/>
-		<stop  offset="1" style="stop-color:#222168"/>
-	</linearGradient>
-	<path class="st2" d="M319,107.3c0,0,307-37.5,431,111.7c1-91.7,0-112.7,0-112.7S509,6.3,319,107.3z"/>
-	<linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="-0.1626" y1="107.412" x2="749.8508" y2="107.412" gradientTransform="matrix(1 -8.174051e-04 8.174051e-04 1 -9.172272e-02 0.3066)">
-		<stop  offset="0" style="stop-color:#222168"/>
-		<stop  offset="4.024143e-02" style="stop-color:#20256E"/>
-		<stop  offset="9.658029e-02" style="stop-color:#1B3281"/>
-		<stop  offset="0.1624" style="stop-color:#12469F"/>
-		<stop  offset="0.2304" style="stop-color:#0760C5"/>
-		<stop  offset="0.3488" style="stop-color:#0386B1"/>
-		<stop  offset="0.4458" style="stop-color:#00A89F"/>
-		<stop  offset="0.4584" style="stop-color:#01A2A2"/>
-		<stop  offset="0.5287" style="stop-color:#0385B1"/>
-		<stop  offset="0.5986" style="stop-color:#0571BC"/>
-		<stop  offset="0.6676" style="stop-color:#0764C3"/>
-		<stop  offset="0.7343" style="stop-color:#0760C5"/>
-	</linearGradient>
-	<path class="st3" d="M749.8,106.3c0.1-0.1-153.2-72.2-433,2c-110.6,29.4-317-24-317-24v33c0,0,109.8,108.7,317-8.5
-	c217.9-123.2,433-8.5,433-8.5V106.3z"/>
-</svg>
-</div>
-<div class="background">
-	<p>Raylee has over 25 years of experience as a Human Resources practitioner, organisational and conflict coach, workplace investigator and facilitator. Raylee’s extensive experience working as a senior practitioner in Employment Relations and Human Resources within the public sector has given her the ability to deeply understand the dynamics of workplace relationships and causes of conflict that can occur in that setting.<br><br>
-
-		Raylee is driven by helping professionals to achieve their goals and this includes helping them to succesfully navigate issues of conflict or dispute. Generally, people come to work and want to enjoy the experience, sometimes things aren’t always plain sailing and people need help to identify and then to work through issues or concerns. Raylee’s experience and candid approach with employees and employers enables positive and  pragmatic solutions to issues or concerns in a forum that provides respectful, safe, and structured mediation or facilitation.<br><br>
-
-		With extensive experience in Human Resources and conflict coaching, Raylee is a trusted expert and works frequently with individuals and groups to help them achieve successful outcomes.<br><br>
-
-		Raylee is also a sought after investigator and graduate of the Association of Workplace Investigator’s Training Institute: www.awi.org.<br><br>
-
-	Raylee undertakes independent investigations for employers providing robust reliable findings and reports without delay.</p>
-</div>
+	<script type="text/javascript" src="DIYJS.js"></script>
 </body>
-<script type="text/javascript" src="SmartHR.js"></script>
 </html>
