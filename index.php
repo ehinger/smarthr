2345bcda9da332996d75e80c4e56762a576a33dd
Tom Creagh <tomcreagh@Toms-iMac.local>
2018-06-03 17:22:14 +1200

    bolstering

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SmartHR - Human Resources Consulting</title>
+	<meta name="description=" content="">
+	<link rel="stylesheet" type="text/css" href="SmartHR.css">
+</head>
+<body>
+<!-- Navigation -->
+
+	<div class="sandwichIcon">
+		<div class="barOne"></div>
+		<div class="barTwo"></div>
+		<div class="barThree"></div>
+	</div>
+
+	<div class="menuBar"></div>
+
+	<div class="links">
+		<ul class="links">
+			<li class="linkOne"></li>
+			<li class="linkTwo"></li>
+			<li class="linkThree"></li>
+		</ul>
+	</div>
+
+	<div class="navigationClose">
+		<div class="lineL"></div>
+		<div class="lineR"></div>
+	</div>
+
+	<div class="returnToTop">
+		<img src="SmartHR_ReturnToTopButton.svg" alt="An arrow pointing upwards inside a circle.">
+	</div>
+
+<!-- Introductory Section -->
+
+	<h1>SmartHR</h1>
+	<h2>Raylee Marfell</h2>
+
+
+</div>
+<div class="background">
+	<p>Raylee has over 25 years of experience as a Human Resources practitioner, organisational and conflict coach, workplace investigator and facilitator. Raylee’s extensive experience working as a senior practitioner in Employment Relations and Human Resources within the public sector has given her the ability to deeply understand the dynamics of workplace relationships and causes of conflict that can occur in that setting.<br><br>
+
+		Raylee is driven by helping professionals to achieve their goals and this includes helping them to succesfully navigate issues of conflict or dispute. Generally, people come to work and want to enjoy the experience, sometimes things aren’t always plain sailing and people need help to identify and then to work through issues or concerns. Raylee’s experience and candid approach with employees and employers enables positive and  pragmatic solutions to issues or concerns in a forum that provides respectful, safe, and structured mediation or facilitation.<br><br>
+
+		With extensive experience in Human Resources and conflict coaching, Raylee is a trusted expert and works frequently with individuals and groups to help them achieve successful outcomes.<br><br>
+
+		Raylee is also a sought after investigator and graduate of the Association of Workplace Investigator’s Training Institute: www.awi.org.<br><br>
+
+	Raylee undertakes independent investigations for employers providing robust reliable findings and reports without delay.</p>
+</div>
+</body>
+<script type="text/javascript" src="SmartHR.js"></script>
+</html>
