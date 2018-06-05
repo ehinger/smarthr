e40500411b3f9525f13c1385d229dfc5f2c8bae4
Tom Creagh <tomcreagh@Toms-iMac.local>
2018-06-05 16:20:45 +1200

    bolstering

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>SmartHR - Human Resources Consulting</title>
 	<meta name="description=" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="SmartHR.css">
 </head>
 <body>
