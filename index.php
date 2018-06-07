659425b95716a8dd6189691853ca1032a9e18b77
Tom Creagh <tomcreagh@Toms-iMac.local>
2018-06-08 10:30:06 +1200

    bolstering

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html>
 <head>
 	<title>SmartHR - Human Resources Consulting</title>
-	<meta name="description=" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="SmartHR.css">
+	<meta name="description=" content="">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <!-- Navigation -->
@@ -14,83 +15,85 @@
 		<div class="barTwo"></div>
 		<div class="barThree"></div>
 	</div> -->
+<div id="wrapper">
+		<div class="menuBar"></div>
 
-	<div class="menuBar"></div>
+		<ul class="links">
+			<li class="linkOne">About</li>
+			<li class="linkTwo">What We Do</li>
+			<li class="linkThree">Contact</li>
+		</ul>
 
-	<ul class="links">
-		<li class="linkOne">About</li>
-		<li class="linkTwo">What We Do</li>
-		<li class="linkThree">Contact</li>
-	</ul>
+		<div class="navigationClose">
+			<div class="lineL"></div>
+			<div class="lineR"></div>
+		</div>
 
-	<div class="navigationClose">
-		<div class="lineL"></div>
-		<div class="lineR"></div>
+		<div class="returnToTop">
+			<img src="SmartHR_ReturnToTopButton.svg" alt="An arrow pointing upwards inside a circle.">
+		</div>
+
+	<!-- Introductory Section -->
+
+		<h1>SmartHR</h1>
+		<h2>Raylee Marfell</h2>
+
+		<div class="SVGImage">
+			<img src="SmartHR_header_image.svg" alt="A flourish of blues">
+		</div>
+	<div class="background">
+		<p>The time has come for me, to put myself out there and be courageous and launch my own business – Smart HR.  However, first off, I would like to thank all of you who have supported, encouraged and believed in me, over the years and finally got me to believe in myself -Thank you very much.
+	Over the years, what I’ve realised about myself, is that the work I’ve most enjoyed is when I’ve worked with individuals, team and organisations, to focus on the things that can make a real difference to workplace wellbeing. This includes fixing a process problem removing obstacles and resolving disagreements.
+	I believe that most people come to work and want to enjoy the experience. I also know and appreciate that sometimes things aren’t always plain sailing and people need help to work through an issue or concern. I know the key to achieving positive and pragmatic solutions is to create a forum that allows respectful, safe and structured conversations. 
+	With over 25+ years’ work experience, in both Public and Private sectors, as a senior practitioner in the Human Resource field I understand the dynamics of workplace relationships and causes of conflict that can occur.  I have become a trusted expert and I frequently work with groups and individuals to help them achieve successful outcomes.
+	Please feel free contact me, if you would like to discuss a workplace challenge that is affecting you and/or others.
+	If I’m not the right person to help, I have an extensive professional network of people who I can recommend.<br><br>
+
+
+	My portfolio includes coaching, investigations and organisational and people development.<br><br> 
+
+	Coaching – Helping people to help themselves
+	My approach is to explore the whole system to understand and explore cause and effect.
+	As a coach I translate observations and work on helping to expand perspectives.
+	I offer Individual and Team Coaching to improve Performance and Resolving Conflict.
+	I believe that coaching offers real time learning, that is tailored to the development needs of individuals and teams.
+	Ongoing coaching provides support for the change process to occur.
+	Coaching conversations challenge and stretch for growth.<br><br>
+
+
+
+
+	Investigations – Getting below the surface
+	I undertake independent workplace investigations for employers providing robust reliable findings and reports- without delay. 
+	I believe we should use the right intervention, to gain maximum impact with minimal harm. To achieve this, we need to move away from adversarial to constructive dialogue and balance humanity with pragmatics
+	I am a graduate of the Association of Workplace Investigator's Training Institute http://www.awi.org/
+	In this space I offer the following;
+		•	Pre investigation - Unpack
+		•	Fact finding
+		•	Unpack what’s actually going on
+		•	Exploring other remedies
+		•	Investigate
+		•	Prompt, Impartial and Through
+		•	Post investigation - Rebuild
+		•	Helping individuals and teams to heal<br><br>
+
+	Organisational and People Development – Real people, Real challenges, Real results
+	My tool box of topics and workshops to navigate the real world include topics on
+		•	Learning to learn
+		•	Change Management
+		•	Organisational and Team reviews
+		•	Understanding relationships
+		•	Developing Conflict competence
+		•	Exploring mindsets
+		•	Difficult conversations
+		•	Courageous conversations 
+		•	Behavior management
+		•	Understanding the link between emotions, behaviors and performance<br><br>
+
+	At the end of the day, I am driven by helping professionals to achieve their goals. If I can help and support individuals and teams to grow and learn by helping them to navigate through challenges, issues, conflict or dispute in the workplace I will have done something useful to improve wellbeing in the workplace and maybe even the world.</p>
 	</div>
-
-	<div class="returnToTop">
-		<img src="SmartHR_ReturnToTopButton.svg" alt="An arrow pointing upwards inside a circle.">
-	</div>
-
-<!-- Introductory Section -->
-
-	<h1>SmartHR</h1>
-	<h2>Raylee Marfell</h2>
-
-	<div class="SVGImage">
-		<img src="SmartHR_header_image.svg" alt="A flourish of blues">
-	</div>
-<div class="background">
-	<p>The time has come for me, to put myself out there and be courageous and launch my own business – Smart HR.  However, first off, I would like to thank all of you who have supported, encouraged and believed in me, over the years and finally got me to believe in myself -Thank you very much.
-Over the years, what I’ve realised about myself, is that the work I’ve most enjoyed is when I’ve worked with individuals, team and organisations, to focus on the things that can make a real difference to workplace wellbeing. This includes fixing a process problem removing obstacles and resolving disagreements.
-I believe that most people come to work and want to enjoy the experience. I also know and appreciate that sometimes things aren’t always plain sailing and people need help to work through an issue or concern. I know the key to achieving positive and pragmatic solutions is to create a forum that allows respectful, safe and structured conversations. 
-With over 25+ years’ work experience, in both Public and Private sectors, as a senior practitioner in the Human Resource field I understand the dynamics of workplace relationships and causes of conflict that can occur.  I have become a trusted expert and I frequently work with groups and individuals to help them achieve successful outcomes.
-Please feel free contact me, if you would like to discuss a workplace challenge that is affecting you and/or others.
-If I’m not the right person to help, I have an extensive professional network of people who I can recommend.<br><br>
-
-
-My portfolio includes coaching, investigations and organisational and people development.<br><br> 
-
-Coaching – Helping people to help themselves
-My approach is to explore the whole system to understand and explore cause and effect.
-As a coach I translate observations and work on helping to expand perspectives.
-I offer Individual and Team Coaching to improve Performance and Resolving Conflict.
-I believe that coaching offers real time learning, that is tailored to the development needs of individuals and teams.
-Ongoing coaching provides support for the change process to occur.
-Coaching conversations challenge and stretch for growth.<br><br>
-
-
-
-
-Investigations – Getting below the surface
-I undertake independent workplace investigations for employers providing robust reliable findings and reports- without delay. 
-I believe we should use the right intervention, to gain maximum impact with minimal harm. To achieve this, we need to move away from adversarial to constructive dialogue and balance humanity with pragmatics
-I am a graduate of the Association of Workplace Investigator's Training Institute http://www.awi.org/
-In this space I offer the following;
-	•	Pre investigation - Unpack
-	•	Fact finding
-	•	Unpack what’s actually going on
-	•	Exploring other remedies
-	•	Investigate
-	•	Prompt, Impartial and Through
-	•	Post investigation - Rebuild
-	•	Helping individuals and teams to heal<br><br>
-
-Organisational and People Development – Real people, Real challenges, Real results
-My tool box of topics and workshops to navigate the real world include topics on
-	•	Learning to learn
-	•	Change Management
-	•	Organisational and Team reviews
-	•	Understanding relationships
-	•	Developing Conflict competence
-	•	Exploring mindsets
-	•	Difficult conversations
-	•	Courageous conversations 
-	•	Behavior management
-	•	Understanding the link between emotions, behaviors and performance<br><br>
-
-At the end of the day, I am driven by helping professionals to achieve their goals. If I can help and support individuals and teams to grow and learn by helping them to navigate through challenges, issues, conflict or dispute in the workplace I will have done something useful to improve wellbeing in the workplace and maybe even the world.</p>
 </div>
+
 </body>
 <script type="text/javascript" src="SmartHR.js"></script>
 </html>
